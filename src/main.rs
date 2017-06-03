966ac34bacda503711302f1bba29af9acd532c04
Chinmay Kousik <chinmaykousik1@gmail.com>
2017-06-03 19:42:53 +0530

    f32 -> f64

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,37 @@
 extern crate mlkit;
+extern crate rand;
 
 use mlkit::sgd;
+use rand::Rng;
 
 fn main() {
-    let inputs = vec![vec![1.0], vec![2.0], vec![3.0], vec![4.0], vec![5.0]];
-    let outputs = vec![7.0, 9.0, 11.0, 13.0, 15.0];
-    let coeff = sgd::sgd_optimizer(&inputs, &outputs, 0.055, 0.9, 15);
-    println!("coefficients : {:?}", coeff);
+    let inputs = gen_rand_2d(10, 1);
+    let outputs = gen_rand_1d(10);
+
+    let coeff = sgd::sgd_optimizer(&inputs, &outputs, 0.05, 0.6, 10);
+    println!("p: {:?}", sgd::curve(&inputs, &coeff));
+    println!("v: {:?}", outputs);
 }
+
+// generate a 2d matrix of m*n with random values
+fn gen_rand_2d (m: usize, n: usize) -> Vec<Vec<f64>> {
+    let mut _rng = rand::thread_rng();
+    let mut _2d = vec![ vec![]; m ]; // initialize 2d vector with m rows
+    for i in 0..m {
+        for _ in 0..n {
+            (_2d[i]).push(_rng.gen::<f64>());
+        }
+    }
+    return _2d;
+}
+
+// generate randomize vector of size m
+fn gen_rand_1d (n: usize) -> Vec<f64> {
+    let mut _v = vec![];
+    let mut _rng = rand::thread_rng();
+    for _ in 0..n {
+        _v.push(_rng.gen::<f64>());
+    }
+    return _v;
+}
+
--- src/sgd.rs
+++ src/sgd.rs
@@ -1,22 +1,24 @@
-// Task : Stochastic Gradient Descent Optimizer
+// f64ask : Stochastic Gradient Descent Optimizer
 // Author : Chinmay Kousik
 // Date : 2-JUN-2017
 // Version : 0.0.1
 //
-// TODO: Improve performance for larger data sets
-// TODO: parallelization
-// TODO: momentum
+// f64ODO: Improve performance for larger data sets
+// f64ODO: parallelization
 //
 // Parallelization:
 // Draw m random samples from the set. Run optimizer on each subset. Average results of each
 // parallel iteration. Results should converge as all subsets are drawn from the same data set.
 extern crate rand;
+extern crate num;
+
 use self::rand::Rng;
 
-use std::{f32, u32};
+use std::{f64, u32};
+
 
 // add 1 to start of each vector
-fn add_bias_term(input : &Vec<Vec<f32>>) -> Vec<Vec<f32>> {
+fn add_bias_term(input : &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
     let mut _result = Vec::new();
     for i in 0..input.len() {
         let mut _c = input[i].clone();
@@ -27,21 +29,21 @@ fn add_bias_term(input : &Vec<Vec<f32>>) -> Vec<Vec<f32>> {
 }
 
 // sgd_diff calculates the difference of the hypothesis and observation at a given point
-// x: Vec<f32> : input vector
-// w: Vec<f32> : weights
-// y: f32 : expected output
-fn sgd_diff(x: &Vec<f32>, w: &Vec<f32>, y: f32) -> f32 {
+// x: Vec<f64> : input vector
+// w: Vec<f64> : weights
+// y: f64 : expected output
+fn sgd_diff(x: &Vec<f64>, w: &Vec<f64>, y: f64) -> f64 {
     let hypothesis = x.iter().zip(w.iter()).fold(0.0, |sum, (xj, wj)| sum + (*xj)*(*wj));
     return hypothesis - y;
 }
 
 // sgd_optimizer_run takes x and y and returns linear regression coefficients
-// x_batch: Vec<Vec<f32>> : inputs
-// y_batch: Vec<f32> : outputs
-// coeff : Vec<f32> : current coefficients
-// lr : f32 : learning rate
-// mom: f32 : momentum
-fn sgd_optimizer_run(x_batch: &Vec<Vec<f32>>, y_batch: &Vec<f32>, coeff: &Vec<f32>, lr: f32, mom: f32) -> Vec<f32> {
+// x_batch: Vec<Vec<f64>> : inputs
+// y_batch: Vec<f64> : outputs
+// coeff : Vec<f64> : current coefficients
+// lr : f64 : learning rate
+// mom: f64 : momentum
+fn sgd_optimizer_run(x_batch: &Vec<Vec<f64>>, y_batch: &Vec<f64>, coeff: &Vec<f64>, lr: f64, mom: f64) -> Vec<f64> {
     // shuffle data set
     let x_b = add_bias_term(x_batch);
     let (x_b, y_b) = shuffle_data_set(&x_b, y_batch);
@@ -62,12 +64,12 @@ fn sgd_optimizer_run(x_batch: &Vec<Vec<f32>>, y_batch: &Vec<f32>, coeff: &Vec<f3
 
 // sgd_optimizer takes x,y, learning rate, and number of epochs and uses stochastic gradient
 // descent to calculate regression coefficients
-// x : Vec<Vec<f32>> : input vector
-// y : Vec<f32> : observed outputs
-// lr: f32 : learning rate of the optimizer
-// mom : f32: momentum
+// x : Vec<Vec<f64>> : input vector
+// y : Vec<f64> : observed outputs
+// lr: f64 : learning rate of the optimizer
+// mom : f64: momentum
 // epochs : u32 : number of times sgd optimization is run
-pub fn sgd_optimizer(x: &Vec<Vec<f32>>, y: &Vec<f32>, lr: f32, mom: f32, epochs: u32) -> Vec<f32>{ 
+pub fn sgd_optimizer(x: &Vec<Vec<f64>>, y: &Vec<f64>, lr: f64, mom: f64, epochs: u32) -> Vec<f64>{ 
     // number of features + bias
     let mut _guess = vec![1.0; x[0].len() + 1];
 
@@ -81,7 +83,7 @@ pub fn sgd_optimizer(x: &Vec<Vec<f32>>, y: &Vec<f32>, lr: f32, mom: f32, epochs:
 }
 
 // Fisher-Yates shuffle algorithm
-fn shuffle_data_set (x_data: &Vec<Vec<f32>>, y_data: &Vec<f32>) -> (Vec<Vec<f32>>, Vec<f32>) {
+fn shuffle_data_set(x_data: &Vec<Vec<f64>>, y_data: &Vec<f64>) -> (Vec<Vec<f64>>, Vec<f64>) {
     let mut _x = x_data.clone();
     let mut _y = y_data.clone();
 
@@ -105,3 +107,12 @@ fn shuffle_data_set (x_data: &Vec<Vec<f32>>, y_data: &Vec<f32>) -> (Vec<Vec<f32>
     return (_x,_y);
 }
 
+// generate predicitions given coefficients
+pub fn curve(inputs: &Vec<Vec<f64>>, coefficients: &Vec<f64>) -> Vec<f64> {
+    let x_b = add_bias_term(inputs);
+    let mut _pred = vec![];
+    for (_,x) in x_b.iter().enumerate() {
+        _pred.push(sgd_diff(x,coefficients,0.0));
+    }
+    return _pred;
+}
